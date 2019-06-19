f1e366aca71ea3d98db67d732a9fd330ee510168
Michael Van Der Velden <mikevdv@vandie.co.uk>
2019-06-19 15:44:24 +0100

    add comments

--- paydays.js
+++ paydays.js
@@ -2,6 +2,7 @@ const dayjs = require('dayjs');
 
 class Paydays {
   constructor(){
+    //Ensures that all functions start from the same point
     this.startdate = dayjs();
   }
 
@@ -10,9 +11,9 @@ class Paydays {
    * @param {number} n The number of months ahead of the current month. Current month is equal to 0
    */
   getNthPayDayObject(n = 0){
-    let payDay = this.startdate.add(n,'month').endOf('month')
-    while(payDay.day() == 0 || payDay.day() == 6){
-      payDay = payDay.subtract(1, 'day');
+    let payDay = this.startdate.add(n,'month').endOf('month') //add 'n' months to the current date and get the final day in the new month
+    while(payDay.day() == 0 || payDay.day() == 6){ //repeat until the day is not a weekend
+      payDay = payDay.subtract(1, 'day'); //take away a single day
     }
     return payDay;
   }
@@ -22,7 +23,7 @@ class Paydays {
    * @param {number} [n=0] The number of months ahead of the current month. Current month is equal to 0
    */
   getNthPayDay(n = 0){
-    return this.getNthPayDayObject(n).format('dddd - DD/MM/YYYY');
+    return this.getNthPayDayObject(n).format('dddd - DD/MM/YYYY');//format the dayJS object as 'DayName - Day/Month/Year`
   }
 
   /**
@@ -31,9 +32,9 @@ class Paydays {
    */
   getNthBonusDayObject(n=0){
     let bonusDay = this.startdate.add(n,'month').date(15);
-    if(bonusDay.day() == 0 || bonusDay.day() == 6){
-      while(bonusDay.day() != 3){
-        bonusDay = bonusDay.add(1, 'day');
+    if(bonusDay.day() == 0 || bonusDay.day() == 6){ //if the day is on a weekend
+      while(bonusDay.day() != 3){ //repeat until the day is a wednsday
+        bonusDay = bonusDay.add(1, 'day'); //add a day
       }
     }
     return bonusDay;
@@ -44,19 +45,19 @@ class Paydays {
    * @param {number} [n=0] The number of months ahead of the current month. Current month is equal to 0
    */
   getNthBonusDay(n = 0){
-    return this.getNthBonusDayObject(n).format('dddd - DD/MM/YYYY');
+    return this.getNthBonusDayObject(n).format('dddd - DD/MM/YYYY');//format the dayJS object as 'DayName - Day/Month/Year`
   }
 
   /**
    * Returns a csv formatted string of all pay and bonus days in the next 12 months
    */
   nextYearAsCSV(){
-    let paydays = new Array(12).fill('').map((v,i) => this.getNthPayDay(i));
-    let bonusdays = new Array(12).fill('').map((v,i) => this.getNthBonusDay(i));;
+    let paydays = new Array(12).fill('').map((v,i) => this.getNthPayDay(i)); //create an empty array of fixed length '12' and fill it with the payday string for the correct indexes
+    let bonusdays = new Array(12).fill('').map((v,i) => this.getNthBonusDay(i)); //create an empty array of fixed length '12' and fill it with the bonusday string for the correct indexes
     
-    let csv = 'Pay Day, Bonus Day';
-    for(let i = 0; i < 12; i++){
-      csv += `\n${paydays[i]}, ${bonusdays[i]}`;
+    let csv = 'Pay Days, Bonus Days'; //start the string with the 2 csv column titles
+    for(let i = 0; i < 12; i++){ //repeat 12 times
+      csv += `\n${paydays[i]}, ${bonusdays[i]}`;//output a payday and a bonusday, seperated by a comma, on a new line
     }
     
     return csv;
